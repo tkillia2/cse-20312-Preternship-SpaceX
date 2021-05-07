5f66475cdd2dba8dad0c5927330e68d49fd04181
Sergio Morales <smorale3@nd.edu>
2021-05-07 02:28:22 -0400

    added usage

--- Week3/main.py
+++ Week3/main.py
@@ -2,15 +2,31 @@
 
 import csv
 import sys
+import os
 from collections import defaultdict
 from collections import OrderedDict
 from funcs import raw_vendor # import from funcs.py  
 from funcs import print_vendor_data # import from funcs.py
 from funcs import category_totals
 
+def usage(status):
+    ''' Display usage message and exit with status. '''
+    progname = os.path.basename(sys.argv[0])
+    print(f'''Usage: {progname} [flags] [vendor (if applicable)]
+    -r      Prints raw data for any or all vendors. Specify vendor with letter A-J or All for all vendors
+    -t      Prints each vendors category totals. No need to specify vendor
+    -g      Calculates grades for each vendor and displays score, GPA and suggestion. No need to specify vendor
+    -s      Prints vendor and suggestion only. Specify vendor with letter A-J or All for all vendors
+    -h      Help, print usage
+By default, {progname} prints one of each type of line.''')
+    sys.exit(status)
+
 def main():
     arguments = sys.argv[1:] # store command line arguments not inluding file name
     # iterate through command line arguments
+    if len(arguments) < 2:
+        usage(1)
+
     while arguments:
         # take first command line argument
         argument = arguments.pop(0)
@@ -22,6 +38,9 @@ def main():
         elif argument == '-t':
             argument = arguments.pop(0)
             category_totals(argument)
-             
+        #elif argument == '-g':
+        #elif argument == '-h':
+            #usage(0) 
+
 if __name__ == '__main__':
     main()
